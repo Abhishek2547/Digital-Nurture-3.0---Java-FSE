f2304a54095bad0f5160b0fab1f5e8e7d9821f88
Abhishek2547 <ababhi3475@gmail.com>
2024-08-05 16:44:55 +0530

    new

--- WEEK 2/Spring Core_Maven/Exercise 1/librarymanagement/src/main/java/com/library/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.library.repository;
-
-public class BookRepository {
-    public void saveBook(String book) {
-        // Logic to save the book
-        System.out.println("Book saved: " + book);
-    }
-}
